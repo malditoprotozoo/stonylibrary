9a5ce3cb5797b282bfc86ebe8d4806dd8c2ccf7f
malditoprotozoo <virodriguez.fe@gmail.com>
2018-01-15 10:28:55 -0300

    Functions and data fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+
   firebase.initializeApp(config);
-  displayData(fanfics, "language", $("#select-lang"));
-  displayData(fanfics, "rating", $("#select-rating"));
-  displayData(fanfics, "universe", $("#select-universe"));
+
+  var selectLang = $("#select-lang");
+  var selectRat = $("#select-rating");
+  var selectUniv = $("#select-universe");
+  var selectGenre = $("#select-genre");
+
+  displayData(fanfics, "language", $(selectLang));
+  displayData(fanfics, "rating", $(selectRat));
+  displayData(fanfics, "universe", $(selectUniv));
+  displayData(fanfics, "mainGenre", $(selectGenre));
+
+  $(".selector").click(function() {
+    $("#fics-container").empty();
+  });
+
+  showFics(fanfics, "language");
+  showFics(fanfics, "rating");
+  showFics(fanfics, "universe");
+  showFics(fanfics, "mainGenre");
 });
 
 // Initialize Firebase
@@ -32,9 +49,65 @@ var displayData = (function(data, key, where) {
     }
   }
   for (var x = 0; x < arr.length; x++) {
-    $(where).append(`<p>` + arr[x] + `</p>`);
+    $(where).append(`<p class="selector">` + arr[x] + `</p>`);
   }
 });
+
+var showFics = (function(data, key) {
+  $(".selector").click(function() {
+    var filterSelected = $(this).html();
+    for (var i = 0; i < data.length; i++) {
+      var item = data[i][key];
+      if (typeof item === "string" && item === filterSelected) {
+        $("#fics-container").append(`<div class="card"><div class="card-header">`
+          + data[i].title + `</div><div class="card-body"><ul class="list-group list-group-flush">
+          <li class="list-group-item">Author: <a href="` + data[i].authorsLink[0] + `">` + data[i].authorsName[0] 
+          + `</a></li><li class="list-group-item">Platform: ` + data[i].platform + `</li><li class="list-group-item">Language: `
+          + data[i].language + `</li>
+          <li class="list-group-item">Summary:<blockquote class="blockquote mb-0">` + data[i].summary + `</blockquote></li>
+          <li class="list-group-item">Genre: ` + data[i].mainGenre + `</li></ul></div></div>`);
+        console.log(data[i].title);
+      } else if (Array.isArray(item)) {
+        for (var n = 0; n < item.length; n++) {
+          if (item[n] === filterSelected) {
+            $("#fics-container").append(`<div class="card"><div class="card-header">`
+              + data[i].title + `</div></div>`);
+          }
+        }
+      }
+    }
+  });
+});
+
+/* 
+$("#fics-container").children().remove();
+var showFics = (function(selector, data, key) {
+  $(selector).click(function() {
+    var selected = $(this).html();
+    for (var i = 0; i < data.length; i++) {
+      var item = data[i][key];
+      if (typeof item === "string" && item === selected) {
+        $("#fics-container").append(`<div class="card"><div class="card-header">`
+          + data[i].title + `</div></div>`);
+        console.log(data[i].title);
+      } else if (Array.isArray(item) && item[0] === selected) {
+        for (var n = 0; n < item.length; n++) {
+          if (item[n] === selected) {
+            $("#fics-container").append(`<div class="card"><div class="card-header">`
+              + data[i].title + `</div></div>`);
+            console.log(data[i].title);
+          }
+        }
+      }
+    }
+  });
+});
+
+
+
+*/
+
+//$("#fics-container").children().remove();
 // var addFic = (function() {
 //   var btnSend = $("#send-data");
 //   $(btnSend).click(function() {
@@ -77,3 +150,4 @@ var displayData = (function(data, key, where) {
 //     })
 //   })
 // })
+

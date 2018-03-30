f9ca8372a2b2833f30812eee8453d28fc882959f
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-30 10:52:10 -0300

    adding 404 component and route

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
+import Err404 from './components/404';
 import './App.css';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
         <Navbar/>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route component={Err404} />
         </Switch>
       </div>
     );
---
+++ src/components/404/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Err404 extends Component {
+  render() {
+    return(
+      <p>404</p>
+    );
+  }
+}
+
+export default Err404;

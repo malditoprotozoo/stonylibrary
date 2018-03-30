4e2f085e3781fe3566dd3560182294131c94415c
Tori Rodríguez <virodriguez.fe@gmail.com>
2018-03-30 13:49:55 -0300

    adding sidebar component

---
+++ src/components/Sidebar/LanguageSelector.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+class LanguageSelector extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: null
+    }
+  }
+  componentWillMount() {
+    fetch('/data/ao3.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+    .then(response => {
+      return response.json();
+    })
+    .then(response => {
+      let langs = [];
+      response.map(fic => {
+        if (langs.indexOf(fic.language) === -1) {
+          langs.push(fic.language);
+        }
+        return langs;
+      })
+      this.setState({
+        data: langs
+      })
+    })
+  }
+  render() {
+    return (
+      <div className="panel panel-default">
+        <div className="panel-heading">
+          <h4 className="panel-title">
+            <a data-toggle="collapse" href="#LanguageSelector">Language</a>
+          </h4>
+        </div>
+        <div className="panel-collapse collapse" id="LanguageSelector">
+          {
+            this.state.data !== null ?
+              this.state.data.map(item => {
+                return (
+                  <div className="panel-body" key={item}>
+                    {item}
+                  </div>
+                );
+              })
+            : <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+          }
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LanguageSelector;
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import LanguageSelector from './LanguageSelector';
+import './index.css';
+// import { Link } from 'react-router-dom';
+
+class Sidebar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fics: null,
+      languages: null,
+      universes: null
+    }
+  }
+  componentWillMount() {
+    fetch('/data/ao3.json', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      }
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(response => {
+        let fics = []
+        response.map(fic => {
+          fics.push(fic);
+          return fic;
+        })
+        this.setState({
+          fics: fics
+        })
+      })
+  }
+  render() {
+    return(
+      <div className="col-12 col-lg-2" id="Sidebar">
+        {
+          this.state.fics !== null ?
+            <div className="panel-group">
+              <LanguageSelector/>
+            </div>
+          : <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
+      }
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
